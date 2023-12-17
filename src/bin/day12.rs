c5b03c1c84d940ec41ad26c5dd528efc7c41623d
Weber Chu <weberchu@amazon.com>
2023-12-17 22:41:41 +0000

    Day 12 part 2 incomplete

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 
 fn main() {
-    let input = include_str!("./input/example.txt");
+    let input = include_str!("./input/day12.txt");
     part1(input);
     part2(input);
 }
@@ -10,14 +10,20 @@ fn part1(input: &str) {
     let mut sum = 0;
 
     for line in input.lines() {
-        println!("----");
+        println!("---- {}", line);
         let split: Vec<&str> = line.split(" ").collect();
         let visual_record = split[0];
         let number_record = split[1];
 
         let contiguous_damaged: Vec<usize> = number_record.split(",").map(|s| s.parse::<usize>().unwrap()).collect();
 
-        let possible_arrangements = possible_arrangements(&visual_record.chars().collect(), &contiguous_damaged);
+        // let possible_arrangements = possible_arrangements(&visual_record.chars().collect(), &contiguous_damaged);
+        let possible_arrangements = possible_arrangements_v2(&visual_record.chars().collect(), &contiguous_damaged);
+        // println!("possible_arrangements = {possible_arrangements}");
+        // println!("possible_arrangements2 = {possible_arrangements2}");
+        // if possible_arrangements != possible_arrangements2 {
+        //     panic!("not equal");
+        // }
 
         sum += possible_arrangements;
     }
@@ -54,13 +60,99 @@ fn part2(input: &str) {
 
         println!("visual_record = {:?}", visual_record.iter().collect::<String>());
         println!("contiguous_damaged = {contiguous_damaged:?}");
-        let possible_arrangements = possible_arrangements(&visual_record, &contiguous_damaged);
+        let possible_arrangements = possible_arrangements_v2(&visual_record, &contiguous_damaged);
         println!("possible_arrangements = {possible_arrangements}");
 
         sum += possible_arrangements;
     }
 
     println!("Part 2: {sum}");
+}
+
+fn possible_arrangements_v2(visual_record: &Vec<char>, contiguous_damaged: &Vec<usize>) -> usize {
+    let right_most_possible_start = right_most_possible_start(visual_record, contiguous_damaged);
+    // println!("visual_record={visual_record:?}, contiguous_damaged={:?}", contiguous_damaged);
+    // println!("right_most_possible_start={right_most_possible_start}");
+
+    let mut possible_arrangements = 0;
+    let first_damaged_group = contiguous_damaged[0];
+
+    for i in 0..right_most_possible_start + 1 {
+        let mut is_possible = true;
+        for j in i..i + first_damaged_group {
+            if visual_record[j] == '.' {
+                is_possible = false;
+                break;
+            }
+        }
+        if contiguous_damaged.len() == 1 {
+            for j in i + first_damaged_group..visual_record.len() {
+                // cannot have more damaged spring after last group
+                if  visual_record[j] == '#' {
+                    is_possible = false;
+                    break;
+                }
+            }
+        } else {
+            if i + first_damaged_group < visual_record.len() && visual_record[i + first_damaged_group] == '#' {
+                is_possible = false;
+            }
+        }
+
+        if is_possible {
+            if contiguous_damaged.len() == 1 {
+                // println!(" p record={:?} finding{:?}@{i} = 1", visual_record.iter().collect::<String>(), contiguous_damaged);
+                possible_arrangements += 1;
+            } else {
+                let remaining_visual_record = Vec::from(visual_record.split_at(i + first_damaged_group + 1).1);
+                let remaining_contiguous_damaged = Vec::from(contiguous_damaged.split_first().unwrap().1);
+
+                // println!("-record={:?} finding{:?}@{i} delegating", visual_record.iter().collect::<String>(), contiguous_damaged);
+                let p = possible_arrangements_v2(&remaining_visual_record, &remaining_contiguous_damaged);
+                // println!(" p record={:?} finding{:?}@{i} = {p}", visual_record.iter().collect::<String>(), contiguous_damaged);
+                possible_arrangements += p;
+            }
+        } else {
+            // println!(" p record={:?} finding{:?}@{i} = 0", visual_record.iter().collect::<String>(), contiguous_damaged);
+        }
+
+        if visual_record[i] == '#' {
+            // cannot skip a damaged spring
+            break;
+        }
+    }
+
+    possible_arrangements
+}
+
+fn right_most_possible_start(visual_record: &Vec<char>, contiguous_damaged: &Vec<usize>) -> usize {
+    let mut right_most_possible_start = visual_record.len() - 1;
+    for (d_i, &d) in contiguous_damaged.iter().enumerate().rev() {
+        let mut is_current_found = false;
+        while !is_current_found && right_most_possible_start >= 0 {
+            let mut is_damaged_found = true;
+            for j in right_most_possible_start + 1 - d..right_most_possible_start + 1 {
+                if visual_record[j] == '.' {
+                    right_most_possible_start -= 1;
+                    is_damaged_found = false;
+                    break;
+                }
+            }
+            if is_damaged_found {
+                is_current_found = true;
+            }
+        }
+
+        if !is_current_found {
+            panic!("impossible arrangement. visual_record={:?}, contiguous_damaged={:?}", visual_record, contiguous_damaged);
+        }
+
+        if d_i > 0 {
+            right_most_possible_start -= d + 1;
+        }
+    }
+
+    right_most_possible_start + 1 - contiguous_damaged[0]
 }
 
 fn possible_arrangements(visual_record: &Vec<char>, contiguous_damaged: &Vec<usize>) -> usize {
